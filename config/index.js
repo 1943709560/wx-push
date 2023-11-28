bef51a260265aa5727a75de03a2a68aafdba8311
1943709560 <152284403+1943709560@users.noreply.github.com>
2023-11-28 20:53:23 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
+  appid: "wxd424677c487c4ba3",
+  appsecret: "ab6e945c8751982d39d56f101473298f",
+  template_id: "72ljiCp5FmELkQoPLJLFm3c3WbVzZUFDUZK92TuCmY8",
   touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
-  first_date: "2022-11-10",
+  city: "珲春",
+  first_date: "2023-11-11",
 };
